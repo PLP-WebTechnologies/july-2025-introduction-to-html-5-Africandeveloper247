b527b6d2a898ed61d258d5573d7c5f7bc7adef8e
Africandeveloper247 <ijebilla@gmail.com>
2025-07-31 15:33:58 +0100

    new file added successfully

---
+++ week-python-ass.py
@@ -0,0 +1,32 @@
+# a python code for a simple calculator
+print("welcome to my smart calculator")
+
+#accepting number and operators
+first_number=int(input("please enter the first number: "))
+
+operator= input ("please enter your operator sign (+, -, *, /): ")
+
+second_number= int(input("please enter the second number: "))
+
+#printing out the results
+
+if operator == "+":
+    result= first_number + second_number
+
+elif operator == "-":
+    result= first_number - second_number
+elif operator == "+":
+    result= first_number * second_number
+elif operator == "/":
+    if second_number !=0:
+        result= first_number / second_number
+else :
+    result = "invalid operator !"
+
+
+#printing result
+print ("result:", result)
+
+
+
+
